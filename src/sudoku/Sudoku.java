7e7f13298a72f00ddfb5bf4fbfa65855144af009
Hershal Rami <hrami7771@inst.hcpss.org>
2019-03-18 12:14:55 -0400

    update

--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -103,7 +103,7 @@ public class Sudoku {
 	public void checkRows() {
 		for(int i = 0; i < board.length; i++) {
 			for(int j = 0; j < board[0].length; j++) {
-				
+				// TODO write this
 			}
 		}
 	}
